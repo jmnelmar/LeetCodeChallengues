9fa1604678860f306d24a146ac93a54a2797bdca
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-02-16 11:50:00 -0800

    adding aceptable soultion to contain duplicates challenge

--- Python/ContainDuplicates.py
+++ Python/ContainDuplicates.py
@@ -9,10 +9,17 @@ def containsDuplicate(nums):
                     return True
     return False
 
+#Sorting the Array
+def containsDuplicate2(nums):
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return True
+    return False
 
 nums = [1,2,3,1]
-print( containsDuplicate(nums) )
+print( containsDuplicate2(nums) )
 nums[:] = [1,2,3,4]
-print( containsDuplicate(nums) )
+print( containsDuplicate2(nums) )
 nums[:] = [1,1,1,3,3,4,3,2,4,2]
-print( containsDuplicate(nums) )
+print( containsDuplicate2(nums) )
