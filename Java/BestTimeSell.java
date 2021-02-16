6ba4ea9f16b1849ae226c4795337fa3c6aeeebea
Nelson Jimenez <jmnel_mar@hotmail.com>
2021-02-15 22:35:28 -0800

    adding sql challenges

--- Java/BestTimeSell.java
+++ Java/BestTimeSell.java
@@ -1,4 +1,5 @@
-public class BestTimeToSell {
+public class BestTimeSell {
+
     public static int maxProfit(int[] prices) {
         int profit = 0;
         
@@ -15,5 +16,5 @@ public class BestTimeToSell {
         int[] arr = {7,1,5,3,6,4};
         System.out.println(BestTimeToSell.maxProfit(arr));
     }
-
+    
 }
